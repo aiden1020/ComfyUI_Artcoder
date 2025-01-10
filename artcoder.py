9e14aa65b2ab5d13d8ad965ed5b7b677a87b8339
aiden1020 <tsztowong@gmail.com>
2025-01-10 15:24:26 +0800

    all update

--- artcoder.py
+++ artcoder.py
@@ -12,6 +12,7 @@ from .src.utils import (
     convert_normalized_tensor_to_np_image,
     convert_pil_to_normalized_tensor,
 )
+
 def optimize_code(
     content_image: torch.Tensor,
     qrcode_image: torch.Tensor,
@@ -27,7 +28,6 @@ def optimize_code(
     code_weight: float = 1e12,
     content_weight: float = 1e8,
     style_weight: float = 1e15,
-    display_loss: bool = True,
 ) -> np.ndarray:
     with torch.inference_mode(False):
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -35,7 +35,7 @@ def optimize_code(
         qrcode_image = qrcode_image.to(device)
         style_image = style_image.to(device)
 
-        x = content_image.clone().detach().to(device).requires_grad_(True)
+        x = content_image.detach().clone().requires_grad_(True)
         optimizer = torch.optim.Adam([x], lr=lr)
         objective_func = ArtCoderLoss(
             module_size=module_size,
@@ -49,59 +49,15 @@ def optimize_code(
             device=device,
         )
 
-        for i in tqdm(range(iterations)):
+        for i in range(iterations):
             optimizer.zero_grad()
             losses = objective_func(x, qrcode_image, content_image, style_image)
-            losses["total"].backward()  # 去掉 retain_graph
-            optimizer.step()  # 執行優化
-            x.data.clamp_(0, 1)  # 限制張量值範圍在 [0, 1]
-
-            if display_loss:
-                tqdm.write(
-                    f"iterations: {i}, " +
-                    ", ".join([f"{k}_loss: {v:.4f}" for k, v in losses.items()])
-                )
-
+            losses["total"].backward(retain_graph=True)
+            optimizer.step()
+            x.data.clamp_(0, 1)
     return add_position_pattern(
         convert_normalized_tensor_to_np_image(x),
         convert_normalized_tensor_to_np_image(qrcode_image),
         module_size=module_size,
         module_num=module_num,
     )
-
-
-def generate_aesthetic_qrcode(
-    qrcode_image: torch.Tensor,
-    content_image: torch.Tensor,
-    style_image: torch.Tensor,
-    module_size: int = 16,
-    module_num: int = 37,
-    iterations: int = 1000,
-    soft_black_value: float = 40 / 255,
-    soft_white_value: float = 220 / 255,
-    error_mask_black_thres: float = 70 / 255,
-    error_mask_white_thres: float = 180 / 255,
-    lr: float = 0.01,
-    code_weight: float = 1e12,
-    content_weight: float = 1e8,
-    style_weight: float = 1e15,
-) -> Image.Image:
-    aesthetic_qrcode = optimize_code(
-        content_image,
-        qrcode_image,
-        style_image,
-        module_size=module_size,
-        module_num=module_num,
-        iterations=iterations,
-        soft_black_value=soft_black_value,
-        soft_white_value=soft_white_value,
-        error_mask_black_thres=error_mask_black_thres,
-        error_mask_white_thres=error_mask_white_thres,
-        lr=lr,
-        code_weight=code_weight,
-        content_weight=content_weight,
-        style_weight=style_weight,
-    )
-
-    return torch.tensor(aesthetic_qrcode)
-
--- nodes.py
+++ nodes.py
@@ -2,14 +2,25 @@ import torch
 import numpy as np
 from PIL import Image
 from .artcoder import *
+from tqdm import tqdm
 def pil2tensor(image):
-    """Converts a PIL image to a PyTorch tensor."""
     return torch.from_numpy(np.array(image).astype(np.float32) / 255.0).unsqueeze(0)
 
 def tensor2pil(tensor):
-    """Converts a PyTorch tensor to a PIL image."""
-    array = (tensor.squeeze(0).numpy() * 255.0).astype(np.uint8)
+    if tensor.ndimension() == 4 and tensor.shape[0] == 1: 
+        tensor = tensor.squeeze(0)
+    if tensor.ndimension() == 3 and tensor.shape[0] == 3:
+        array = (tensor.permute(1, 2, 0).cpu().numpy() * 255.0).astype(np.uint8)
+    elif tensor.ndimension() == 3 and tensor.shape[2] == 3:
+        array = (tensor.cpu().numpy() * 255.0).astype(np.uint8)
+    elif tensor.ndimension() == 3 and tensor.shape[0] == 1:
+        array = (tensor.squeeze(0).cpu().numpy() * 255.0).astype(np.uint8)
+    elif tensor.ndimension() == 2:
+        array = (tensor.cpu().numpy() * 255.0).astype(np.uint8)
+    else:
+        raise ValueError(f"Unsupported tensor shape: {tensor.shape}")
     return Image.fromarray(array)
+
 
 class ArtCoder:
 
@@ -19,10 +30,10 @@ class ArtCoder:
             "required": {
                 "content_images": ("IMAGE",), 
                 "qr_image": ("IMAGE",), 
+                "style_image": ("IMAGE",), 
                 "content_scaling": ("FLOAT", {"default": 1.0, "min": 0, "max": 10}),
                 "qr_scaling": ("FLOAT", {"default": 1.0, "min": 0, "max": 10}),
-                "resize_width": ("INT", {"default": 512, "min": 1, "max": 2048}),
-                "resize_height": ("INT", {"default": 512, "min": 1, "max": 2048}),
+                "style_scaling": ("FLOAT", {"default": 1.0, "min": 0, "max": 10}),
                 "iteration": ("INT", {"default": 500, "min": 1, "max": 2048}),
             }
         }
@@ -31,45 +42,31 @@ class ArtCoder:
     FUNCTION = "generate_QR_images"
     CATEGORY = "Image Processing"
 
-    def generate_QR_images(self, content_images,qr_image,content_scaling,qr_scaling,resize_width,resize_height,iteration,):
+    def generate_QR_images(self, content_images,qr_image,style_image,content_scaling,qr_scaling,style_scaling,iteration,):
 
         resized_images = []
-        for image_tensor in content_images:
-            style_image_path = "custom_nodes/ComfUI_Artcoder/images/style.jpg"
-            output_path = "custom_nodes/ComfUI_Artcoder/results/image.jpg"
-
+        for image_tensor in tqdm(content_images):
             qrcode_side_len = 16 * 37
             qrcode_size = (qrcode_side_len, qrcode_side_len)
-
             content_image = tensor2pil(image_tensor).resize(qrcode_size, Image.LANCZOS)
             qrcode_image = tensor2pil(qr_image).resize(qrcode_size, Image.LANCZOS)
-            style_image = Image.open(style_image_path).resize(qrcode_size, Image.LANCZOS)
+            style_image = tensor2pil(style_image).resize(qrcode_size, Image.LANCZOS)
 
             content_image = convert_pil_to_normalized_tensor(content_image)
             qrcode_image = convert_pil_to_normalized_tensor(qrcode_image)
             qrcode_image = image_binarize(qrcode_image)
             style_image = convert_pil_to_normalized_tensor(style_image)
 
-            result = generate_aesthetic_qrcode(
+            result = optimize_code(
                 qrcode_image=qrcode_image,
                 content_image=content_image,
                 style_image=style_image,
-                module_size=16,
-                module_num=37,
                 iterations=iteration,
-                soft_black_value=40 / 255,
-                soft_white_value=220 / 255,
-                error_mask_black_thres=70 / 255,
-                error_mask_white_thres=180 / 255,
-                lr=0.01,
-                code_weight=4e9 * qr_scaling,
-                content_weight=1e12 * content_scaling,
-                style_weight=0,
+                code_weight=1e12 * qr_scaling,
+                content_weight=1e8 * content_scaling,
+                style_weight=1e15 * style_scaling,
             )
-            pil_image = tensor2pil(result)
-            resized_pil_image = pil_image.resize((resize_width, resize_height), Image.LANCZOS)
-            # Convert back to tensor
-            resized_images.append(pil2tensor(resized_pil_image))
-            
-        # Stack resized images into a single tensor batch
+
+            pil_image = tensor2pil(torch.tensor(result))
+            resized_images.append(pil2tensor(pil_image))            
         return (torch.cat(resized_images, dim=0),)
